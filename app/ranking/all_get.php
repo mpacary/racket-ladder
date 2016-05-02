9998c84e8e2bd42546068116a2efec734854e0cf
Frosty-Z <maxime.pacary@free.fr>
2016-05-02 23:11:15 +0200

    rankings : changed position for mixed doubles on page showing all results at once

--- app/ranking/all_get.php
+++ app/ranking/all_get.php
@@ -13,20 +13,22 @@
 
 <div class="row">
 
-  <div class="col-xs-6 col-centered">
-    <?php require 'app/ranking/DX_get.php'; ?>
-  </div>
-
-</div>
-
-<div class="row">
-
   <div class="col-xs-6">
     <?php require 'app/ranking/SD_get.php'; ?>
   </div>
 
   <div class="col-xs-6">
     <?php require 'app/ranking/SH_get.php'; ?>
+  </div>
+
+</div>
+
+<hr style="page-break-after: always;" />
+
+<div class="row">
+
+  <div class="col-xs-6 col-centered">
+    <?php require 'app/ranking/DX_get.php'; ?>
   </div>
 
 </div>
